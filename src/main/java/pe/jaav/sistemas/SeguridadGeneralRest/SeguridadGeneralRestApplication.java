f5175ce0bc3f5daada61a93b4138d9f76e62d21c
Jose Arauco <araucovillar@gmail.com>
2017-09-11 01:15:33 -0500

    add config eureka

--- src/main/java/pe/jaav/sistemas/SeguridadGeneralRest/SeguridadGeneralRestApplication.java
+++ src/main/java/pe/jaav/sistemas/SeguridadGeneralRest/SeguridadGeneralRestApplication.java
@@ -5,15 +5,16 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 
 
 @SpringBootApplication(scanBasePackages={"pe.jaav.sistemas"})
-//@EntityScan(basePackages={"pe.royalsystems.springerp.model"})
 @EnableConfigurationProperties
 @EnableScheduling
 @EnableCaching
+@EnableDiscoveryClient
 public class SeguridadGeneralRestApplication {
 
 	public static void main(String[] args) {
