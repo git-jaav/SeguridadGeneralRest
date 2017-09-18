2703128b746beb783063631bc812aff1b6a01e76
Jose Arauco <araucovillar@gmail.com>
2017-09-17 23:54:44 -0500

    arreglos profiles , eureka ...y *.yml disabled

--- src/main/java/pe/jaav/sistemas/SeguridadGeneralRest/SeguridadGeneralRestApplication.java
+++ src/main/java/pe/jaav/sistemas/SeguridadGeneralRest/SeguridadGeneralRestApplication.java
@@ -6,6 +6,8 @@ import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.cloud.netflix.zuul.EnableZuulServer;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 
