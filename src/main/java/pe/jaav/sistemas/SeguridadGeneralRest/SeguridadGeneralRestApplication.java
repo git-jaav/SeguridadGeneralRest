75d349e6548081cfb8e27de335f85de2aa4a55bf
Jose Arauco <araucovillar@gmail.com>
2017-11-02 00:12:06 -0500

    Add Swgger component and swagger UI and Config class to use it to documentation

--- src/main/java/pe/jaav/sistemas/SeguridadGeneralRest/SeguridadGeneralRestApplication.java
+++ src/main/java/pe/jaav/sistemas/SeguridadGeneralRest/SeguridadGeneralRestApplication.java
@@ -2,12 +2,9 @@ package pe.jaav.sistemas.SeguridadGeneralRest;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
-import org.springframework.cloud.netflix.zuul.EnableZuulServer;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 
@@ -16,7 +13,7 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @EnableConfigurationProperties
 @EnableScheduling
 @EnableCaching
-@EnableDiscoveryClient
+//@EnableDiscoveryClient
 public class SeguridadGeneralRestApplication {
 
 	public static void main(String[] args) {
