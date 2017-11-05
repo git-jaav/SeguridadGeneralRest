bd5b8cb6178e08b4e7feab73a976a7deb420d046
Jose Arauco <araucovillar@gmail.com>
2017-11-04 20:04:50 -0500

    fixed components and sets only as an API rest based on Springboot

--- src/main/java/pe/jaav/sistemas/SeguridadGeneralRest/SeguridadGeneralRestApplication.java
+++ src/main/java/pe/jaav/sistemas/SeguridadGeneralRest/SeguridadGeneralRestApplication.java
@@ -4,7 +4,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.cache.annotation.EnableCaching;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 
@@ -13,7 +12,6 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @EnableConfigurationProperties
 @EnableScheduling
 @EnableCaching
-//@EnableDiscoveryClient
 public class SeguridadGeneralRestApplication {
 
 	public static void main(String[] args) {
