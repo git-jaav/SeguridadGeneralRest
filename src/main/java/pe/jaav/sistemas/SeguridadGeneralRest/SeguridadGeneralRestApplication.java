f93a727f3e3fafb750d57e5afd5d2fe70efa06ff
Jose Arauco <araucovillar@gmail.com>
2018-07-15 23:01:21 -0500

    Add config and componentes of Spring CLoud Setting and Eureka

--- src/main/java/pe/jaav/sistemas/SeguridadGeneralRest/SeguridadGeneralRestApplication.java
+++ src/main/java/pe/jaav/sistemas/SeguridadGeneralRest/SeguridadGeneralRestApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 
@@ -12,6 +13,7 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @EnableConfigurationProperties
 @EnableScheduling
 @EnableCaching
+@EnableDiscoveryClient
 public class SeguridadGeneralRestApplication {
 
 	public static void main(String[] args) {
